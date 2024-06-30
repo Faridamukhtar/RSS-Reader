1cdaf0167a27799dbdfae6e393f0fff9bb18b4ce
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-06-30 20:07:39 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Htmx;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddAntiforgery();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
