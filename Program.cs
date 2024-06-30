d567b24b4d437fdf048814ccb1d9c4f98bb1cd40
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-06-30 21:33:07 +0300

    updates

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-﻿using Htmx;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Antiforgery;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Dapper;
+using System.ServiceModel.Syndication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using System.Xml;
+using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,5 +21,75 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapRazorPages();
+
+var _connectionString = "Data Source=RSS-db.db";
+
+var connection = new SqliteConnection(_connectionString);
+connection.Open();
+
+
+app.MapGet("/", (HttpContext context) =>
+{
+    return Results.File("index.html", "text/html");
+});
+
+app.MapGet("/home", (HttpContext context) =>
+{
+    string html =
+        $"""
+        <div class="text-center mt-5 pt-5">
+            <em style="font-size:6rem;" class="em-black">Turbo</em><em style="font-size:6rem;" class="em-purple">Feeds</em>
+        </div>
+
+        <div class="text-center">
+            Revolutionizing Your Browsing Experience
+        </div>
+        """;
+
+    return Results.Content(html, "text/html");
+});
+
+
+app.MapGet("/signup", (HttpContext context) =>
+{
+    string html =
+        $"""
+        <div class="text-center">
+        SignUp content
+        </div>
+        """;
+
+    return Results.Content(html, "text/html");
+
+});
+
+app.MapGet("/login", (HttpContext context) =>
+{
+    string html = $"""
+    <div class="text-center">
+    Login content
+    </div>
+    """;
+
+    return Results.Content(html, "text/html");
+
+});
+
+
+//app.MapPost("/signup", async (Testimonial testimonial) =>
+//{
+//    try
+//    {
+//        var sql = @"INSERT INTO user (username, password) 
+//                    VALUES (@Name, @DestinationName, @Rating, @Review)";
+//        await connection.ExecuteAsync(sql, testimonial);
+//        return Results.Ok();
+//    }
+//    catch (Exception ex)
+//    {
+//        return Results.Problem(ex.Message);
+//    }
+//});
+
 
 app.Run();
